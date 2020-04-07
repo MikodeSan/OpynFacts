a24cdad7ca77ff6fa1a0480e3300a7ec8b9a3c9f
tousmi01 <mickael.toussaint@somfy.com>
2020-04-07 21:54:17 +0200

    deploy

--- opynfact_web_project/opynfact_web_project/settings.py
+++ opynfact_web_project/opynfact_web_project/settings.py
@@ -21,18 +21,19 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'rrkt%ps7yqw*)o6gol*i(@c)ciu_7+f)p1h-!-d@xev!wd5yv+'
-
 # SECURITY WARNING: don't run with debug turned on in production!
 if os.environ.get('ENV') == 'PRODUCTION':
 
     DEBUG = False
     ALLOWED_HOSTS = ['zopynfacts.herokuapp.com']
+
 else:
 
     DEBUG = True
     ALLOWED_HOSTS = []
+
+    # SECURITY WARNING: keep the secret key used in production secret!
+    SECRET_KEY = 'rrkt%ps7yqw*)o6gol*i(@c)ciu_7+f)p1h-!-d@xev!wd5yv+'
 
 
 # Application definition
@@ -94,23 +95,32 @@ WSGI_APPLICATION = 'opynfact_web_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+if os.environ.get('ENV') == 'PRODUCTION':
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql', # on utilise l'adaptateur postgresql
-#         'NAME': 'opynfacts', # le nom de notre base de donnees creee precedemment
-#         'USER': 'opynfacts_adm', # attention : remplacez par votre nom d'utilisateur
-#         'PASSWORD': '',
-#         'HOST': '',
-#         'PORT': '5432',
-#     }
-# }
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql', # on utilise l'adaptateur postgresql
+            'NAME': 'opynfacts', # le nom de notre base de donnees creee precedemment
+            'USER': 'opynfacts_adm', # attention : remplacez par votre nom d'utilisateur
+            'PASSWORD': '',
+            'HOST': '',
+            'PORT': '5432',
+        }
+    }
+
+    db_from_env = dj_database_url.config(conn_max_age=500)
+    DATABASES['default'].update(db_from_env)
+
+
+else:
+    
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
+
 
 # AUTH_USER_MODEL = 'account.ZUser'
 
@@ -171,5 +181,5 @@ if os.environ.get('ENV') == 'PRODUCTION':
     # https://warehouse.python.org/project/whitenoise/
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-    db_from_env = dj_database_url.config(conn_max_age=500)
-    DATABASES['default'].update(db_from_env)
+
+
