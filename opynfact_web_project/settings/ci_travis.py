7dbd8c86c030d1d63e6ca863852e149d9401315c
mtt <miket972@gmail.com>
2020-08-11 13:05:19 +0200

    test

--- opynfact_web_project/settings/ci_travis.py
+++ opynfact_web_project/settings/ci_travis.py
@@ -1,5 +1,21 @@
 from . import *
 
+SECRET_KEY = 'tete_a_toto'
+DEBUG = False
+ALLOWED_HOSTS = []
+
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.middleware.locale.LocaleMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
+]
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
@@ -10,3 +26,12 @@ DATABASES = {
         'PORT': '',
     },
 }
+
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
+
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'staticfiles')
+
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_DIR, 'staticfiles'),       #'static'
+)
