880e3682f68dcedaaed953dc30377ebbd259a93d
mtt <miket972@gmail.com>
2020-08-17 23:06:38 +0200

    test log

--- product/cron.py
+++ product/cron.py
@@ -1,3 +1,23 @@
+import logging
+
+
+
+# Get an instance of a logger
+logger = logging.getLogger(__name__)
+
+
 def my_scheduled_job():
+
+    logger.info('User query', exc_info=True, extra={
+        # Optionally pass a request and we'll grab any information we can
+        'request': '1',
+        })
+
+    logger.exception('test'.format(user_query), exc_info=True, extra={
+        # Optionally pass a request and we'll grab any information we can
+        'request': 'toto',
+        })
+
     print('Hello Cron Job')
+    a = 1 / 0
     pass
