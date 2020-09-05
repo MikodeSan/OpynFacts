56dbf0a3a0fb10cc666d06cf72a5e4f2c56f9397
mtt <miket972@gmail.com>
2020-09-05 11:59:35 +0200

    test 6

--- opynfact_web_project/settings/production.py
+++ opynfact_web_project/settings/production.py
@@ -122,6 +122,6 @@ CRONJOBS = [
     # ('*/3 * * * *', 'migrate', '>> /tmp/sch_job.log'),
     # ('*/3 * * * *', echo "la tete a toto", '>> /tmp/sch_job2.log'),
     # ('*/10   * * * *', 'django.core.management.call_command', ['initializedatabase 0']),
-    ('*/15 * * * *', 'django.core.management.call_command', ['initializedatabase 0'], {}, '>> /tmp/backups_3.log'),
+    ('*/15 * * * *', 'django.core.management.call_command', ['initializedatabase'], {}, '>> /tmp/backups_3.log'),
     # ('*/20 * * * *', 'product.management.commands.initializedatabase', ['0'], '> /home/miket2/backups/last_sunday_auth_backup.json'),
 ]
