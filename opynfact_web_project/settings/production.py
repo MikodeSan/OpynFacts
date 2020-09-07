2380c3a914f3c1ab5f706d993c11c1f642f57ca0
mtt <miket972@gmail.com>
2020-09-07 21:58:04 +0200

    test 13

--- opynfact_web_project/settings/production.py
+++ opynfact_web_project/settings/production.py
@@ -119,8 +119,10 @@ sentry_sdk.init(
 
 CRONJOBS = [
     ('* * * * *', 'product.cron.my_scheduled_job', [], {}, '>> /tmp/djg_cron_test_prd.log'),
+    ('* * * * *', 'crony.my_scheduled_job_2', '>> /tmp/djg_cron_test_2_prd.log'),
     # ('*/3 * * * *', echo "la tete a toto", '>> /tmp/sch_job2.log'),
     ('*/1 * * * *', 'django.core.management.call_command', ['initializedatabase'], '>> /tmp/djg_cron_opnfct_initdata_prd.log'),
     # ('*/15 * * * *', 'django.core.management.call_command', ['initializedatabase', 0], '>> /tmp/djg_cron_opnfct_initdata_prd.log'),
     # ('*/20 * * * *', 'product.management.commands.initializedatabase', [0]'>> /tmp/djg_cron_opnfct_initdata_prd.log'),
+    ('* * * * *', 'django.core.management.call_command', ['dumpdata', 'auth'], {'indent': 4}, '> /tmp/auth_backup.json'),
 ]
